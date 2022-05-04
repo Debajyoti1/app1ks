02b1ad06e9c539faa87232ce0bfd5836098f1f6e
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2022-05-04 23:26:19 +0530

    first commit

---
+++ src/main/java/com/debu/ms/app1/controller.java
@@ -0,0 +1,27 @@
+package com.debu.ms.app1;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/")
+	public HashMap<String, String> home() {
+		// return "app1 from port "+env.getProperty("local.server.port");
+		HashMap<String, String> a = new HashMap<String, String>();
+		a.put("type", "app1");
+		a.put("port", env.getProperty("local.server.port"));
+		a.put("version", "v1");
+		a.put("hostname", env.getProperty("HOSTNME"));
+		return a;
+	}
+
+}
